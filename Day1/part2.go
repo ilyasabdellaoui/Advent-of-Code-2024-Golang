4d01a0280e791dcb476462076a24774c4246137e
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2024-12-03 01:19:56 +0100

    [DAY1] part2: answer riddle part2 & create go package to read puzzle input

---
+++ Day1/part2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+        "fmt"
+        "strings"
+        "strconv"
+        
+        "ilyasabdell.me/advent-code/PuzzleReader"
+)
+
+func main(){
+    puzzleInput := PuzzleReader.ReadPuzzle()
+    
+    var col1 []int
+    var col2 []int
+    
+    lines := strings.Split(puzzleInput,"\n")
+    for _, line := range lines {
+        col := strings.Fields(line)
+        
+        if len(col) != 2 {
+            continue
+        }
+        
+        colVal1, err1 := strconv.Atoi(col[0])
+        colVal2, err2 := strconv.Atoi(col[1])
+        
+        if err1 != nil || err2 != nil{
+            continue
+        }
+        
+        col1 = append(col1, colVal1)
+        col2 = append(col2, colVal2)
+    }
+    
+    var simScore int
+    for _, val := range(col1) {
+        simScore += val*countOcc(col2, val)
+    }
+    
+    fmt.Println(simScore)
+}
+
+func countOcc(slice []int, val int) int {
+    var count int
+    for _, v := range slice {
+        if v == val {
+            count += 1
+        }
+    }
+    return count
+}
---
+++ PuzzleReader/PuzzleReader.go
@@ -0,0 +1,51 @@
+package PuzzleReader
+
+import (
+        "fmt"
+        "net/http"
+        "io"
+        "os"
+          
+        "github.com/joho/godotenv"
+)
+
+var client = http.Client{}
+func ReadPuzzle() string {
+    url := "https://adventofcode.com/2024/day/1/input"
+    err := godotenv.Load(".env")
+    if err != nil {
+      fmt.Println("Ops, can't load env file: ", err)
+      return ""
+    }
+    
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        fmt.Println("Ops, can't make request: ", err)
+        return ""
+    }
+    
+    cookies := []http.Cookie{
+      {Name: "session", Value: os.Getenv("SESSION")},
+    }
+    
+    for _, cookie := range cookies {
+        req.AddCookie(&cookie)
+    }
+    
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Ops, can't fetch  puzzle input: ", err)
+        return ""
+    }
+
+    defer resp.Body.Close()
+    
+    bodyResp, err := io.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Ops, can't read the puzzle body: ", err)
+        return ""
+    } 
+    
+    puzzleInput := string(bodyResp)
+    return puzzleInput
+}
